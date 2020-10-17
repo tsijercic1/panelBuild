f0d1a6b355b507a6beeee5eab7fde2347282ac44
Tarik Sijercic <tsijercic1@etf.unsa.ba>
2020-10-17 13:43:05 +0200

    Build 13:43 17.10.2020

--- assets/js/chunk-2d22d746.64c669b2.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d22d746"],{f820:function(n,e,t){"use strict";t.r(e);var a=function(){var n=this,e=n.$createElement,t=n._self._c||e;return t("v-container",[t("h1",[n._v("About")])])},u=[],c=t("2877"),o=t("6544"),r=t.n(o),l=t("a523"),i={},s=Object(c["a"])(i,a,u,!1,null,null,null);e["default"]=s.exports;r()(s,{VContainer:l["a"]})}}]);
-//# sourceMappingURL=chunk-2d22d746.64c669b2.js.map
